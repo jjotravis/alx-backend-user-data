f94034b476c818e0a7e082819ed5ad1b19c7ed74
Travis <ondegotravis@gmail.com>
2024-07-12 00:19:20 +0300

    Session Auth first 3 tasks

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Inherits from Auth module
+"""
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+from uuid import uuid4
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """
+    Class for Session auth
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates session id for user
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns user id based on session
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
