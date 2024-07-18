fbab956c11b6d7d20d771406a8f6d1ae8c6e2e0d
Travis <ondegotravis@gmail.com>
2024-07-18 23:13:08 +0300

    Added redirect to logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,7 +57,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return "/"
+        return redirect("/")
     else:
         abort(403)
 
